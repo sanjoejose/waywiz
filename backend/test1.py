317dac6c773c838f73be4047ea2ce81ca9d7e41f
Sanjoe Jose <sanjoejose2025@cs.ajce.in>
2024-10-06 21:56:52 +0530

    python test 1

---
+++ backend/test1.py
@@ -0,0 +1,96 @@
+import sys
+
+def dijkstra_algorithm(G, S, T):
+    L=[]
+    n=len(G)
+
+    for i in range(len(T)):
+        nodes_path = []
+        distance = [sys.maxsize] * n
+        used = [False] * n 
+        previous = [None] * n 
+        
+        distance[S] = 0
+
+        while True:
+            min_distance = sys.maxsize #infinity
+            min_node = None
+            
+            for m in range(n):
+                if not used[m] and distance[m] < min_distance:
+                    min_distance = distance[m]
+                    min_node = m
+
+            if min_distance == sys.maxsize:
+                break  
+
+            used[min_node] = True  
+
+            
+            for l in range(n):
+                if G[min_node][l] > 0:
+                    shortest_to_min_node = distance[min_node]
+                    distance_to_next_node = G[min_node][l]
+                    total_distance = shortest_to_min_node + distance_to_next_node
+                    
+                    
+                    if total_distance < distance[l]:
+                        distance[l] = total_distance
+                        previous[l] = min_node
+
+        
+        if distance[T[i]] == sys.maxsize: 
+            return None  
+
+        current_node = T[i]
+        while current_node is not None: 
+            nodes_path.append(current_node) 
+            current_node = previous[current_node] 
+        
+        
+        col1 = S 
+        for j in range(len(nodes_path) - 1, -1, -1): 
+            L.append(nodes_path[j])  
+            col1 = nodes_path[j]
+
+        S = T[i]  
+
+    return L 
+
+
+
+G = [
+    # 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], # 0
+    [ 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 1
+    [ 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 2
+    [ 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 3
+    [ 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 4
+    [ 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], # 5
+    [ 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 6
+    [ 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 7
+    [ 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 8
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 9
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 10
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0], # 11
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0], # 12
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0], # 13
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 14
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 15
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0], # 16
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0], # 17
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0], # 18
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0], # 19
+    [ 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], # 20
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0], # 21
+    [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1], # 22
+    [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]  # 23
+]
+
+S = 1  
+T = [15, 23]  
+
+path = dijkstra_algorithm(G, S, T)
+print("Path:", path)
+
+
