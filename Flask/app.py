a46e02a9cdee58ec910d17688378678b6bbda734
ruth-tennyson <ruth.tennyson.20@gmail.com>
2025-01-07 22:55:17 +0530

    1/7

--- Flask/app.py
+++ Flask/app.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 
 # Define the graph
 graph = {
-    100: [ ( 1136, 'right', 122), (250, 'straight',123)],
+    100: [ ( 1136, 'right', 122), (250, 'straight',123) (200, 'up',200)],
     101: [(1175, 'straight', 102)],
     102: [(1032, 'right', 103), (1032, 'left', 104), (775, 'straight', 105), (1175, 'straight',101)],
-    103: [(1032, 'straight', 102)],
-    104: [(1032, 'straight', 102)],
+    103: [(1032, 'straight', 102), (200, 'up', 203)],
+    104: [(1032, 'straight', 102), (200, 'up',204)],
     105: [(250, 'left', 120), (250,'right', 106),(775,'straight',102)],
     106: [(880, 'straight', 107), (250, 'left', 105)],
     107: [(523, 'straight', 108), (880, 'straight', 106)],
     108: [(523, 'straight', 107), (1136, 'straight', 109), (1050, 'straight', 110)],
     109: [(1136, 'right', 108), (250, 'straight', 110)],
-    110: [(250, 'straight', 109), (1050, 'left', 108)],
+    110: [(250, 'straight', 109), (1050, 'left', 108), (200, 'up',210)],
     120: [ (250, 'right', 105), (880, 'straight', 121)],
     121: [ ( 880, 'straight', 120), (350, 'straight', 122)],
     122: [ ( 350, 'straight', 121), (1136, 'straight', 123), (1136, 'straight', 100)],
@@ -23,20 +23,20 @@ graph = {
 }
 
 graph2 = {
-    200: [ (350, 'right', 236), (875, 'straight',235)],
+    200: [ (350, 'right', 236), (875, 'straight',235), (200, 'down',100)],
     236: [ (700,'straight',237),(350,'straight',200)],
     237: [ (700,'straight',236)],
     235: [ (875,'straight',200),(523,'straight',234)],
     234: [ (523,'straight',235),(150,'straight',233)],
     233: [ (150,'straight',234),(1400,'straight',232)],
     232: [ (625,'straight',204),(1400,'straight',233),(1050,'straight',226)],
-    204: [ (625,'straight',232)],
+    204: [ (625,'straight',232),(200, 'down',104)],
     226: [(625,'straight',203),(1050,'straight',232),(1050,'straight',227)],
-    203: [ (625,'straight',226)],
+    203: [ (625,'straight',226), (200, 'down',103)],
     227: [ (150,'straight',228),(1050,'straight',226)],
     228: [(150,'straight',227),(529,'straight',229)],
     229: [(875,'straight',210),(529,'straight',228)],
-    210: [(875,'straight',229),(1050,'straight',230)],
+    210: [(875,'straight',229),(1050,'straight',230),(200,'down',110)],
     230: [(1050,'straight',210)]
 }
 
