8ff4a490f616bfc3318a9c7912f928388f4d7aa8
Sanjoe Jose <sanjoejose2025@cs.ajce.in>
2025-01-23 14:38:44 +0530

    map

--- qrcode/qrcodegen.py
+++ qrcode/qrcodegen.py
@@ -1,6 +1,6 @@
 import qrcode
 
-n = "148"  # Data to encode in the QR code
+n = "126"  # Data to encode in the QR code
 img = qrcode.make(n)  # Generate the QR code
-img.save('hod_it.png')  # Save the QR code as an image file
+img.save('CS FACULTY ROOM 1.png')  # Save the QR code as an image file
 
